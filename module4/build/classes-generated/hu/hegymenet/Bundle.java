5064881ff9d976e4efb70d8cc0699be38171de28
helasz <helasz@gmail.hu>
2017-04-10 09:43:52 +0200

    Add files via upload

---
+++ module4/build/classes-generated/hu/hegymenet/Bundle.java
@@ -0,0 +1,13 @@
+package hu.hegymenet;
+/** Localizable strings for {@link hu.hegymenet}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>multiui</i>
+     * @see MultUIWizardIterator
+     */
+    static String MultUI_displayName() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "MultUI_displayName");
+    }
+    private Bundle() {}
+}
